75ece6c11be0c3ea70c1db5e3b1586d07c2b7cc1
tarek hachani <tarek.hachani.ext@francetv.fr>
2020-08-16 17:18:27 +0200

    add example call to WeatherManager from DemoApp

--- DemoApp/ContentView.swift
+++ DemoApp/ContentView.swift
@@ -7,15 +7,29 @@
 //
 
 import SwiftUI
+import WeatherCore
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        Text("Weather Demo")
+            .onAppear() {
+                fetchWeatherData()
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+private func fetchWeatherData() {
+    let weatherManager = WeatherDataManager()
+
+    let latitude = 48.2
+    let longitude = 2.3
+
+    weatherManager.weatherForLocation(latitude: latitude, longitude: longitude) { result in
+        switch result {
+        case .success(let weather):
+            print(weather)
+        case .failure(let error):
+            print("\(error.localizedDescription)")
+        }
     }
 }
